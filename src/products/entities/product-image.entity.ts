9f60fb914964ba6a5f4c1b831070da4ca5f7315f
Pablo_Herrera <pabloemi1998@gmail.com>
2025-03-12 16:13:49 -0500

    product-image entity

---
+++ src/products/entities/product-image.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Product } from '.';
+
+@Entity()
+export class ProductImage {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  url: string;
+
+  @ManyToOne(
+    ()=> Product,
+    (product)=>product.images,
+  )
+  product:Product
+}
