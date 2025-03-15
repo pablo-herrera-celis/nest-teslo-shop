5c8cf20dc6b972c460f4a5726194402937db9ca9
Pablo_Herrera <pabloemi1998@gmail.com>
2025-03-15 09:45:48 -0500

    Crando modulo Auth, ya esta conectado a la base de datos y validado.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/auth.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+
+      await this.userRepository.save(user);
+
+      return user;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
+
+  private handleDBExceptions(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    throw error;
+  }
+}
